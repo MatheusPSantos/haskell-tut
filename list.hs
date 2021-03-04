ff903894d5ae4346e9099b661786f08f0a5cd97b
Matheus Pereira dos Santos <msantos.2015@alunos.utfpr.edu.br>
2021-03-03 23:56:32 -0300

    starting tuples

--- list.hs
+++ list.hs
@@ -60,3 +60,13 @@ cycleList = take 10 (cycle [1,2,4])
 listTimes2 = [x * 2 | x <- [1..10]]
 
 listTimes3 = [x * 3 | x <- [1..20], x * 3 <= 50]
+divisBy9N13 = [x | x <- [1..500], x `mod` 13 == 0, x `mod` 9 == 0]
+sortedList = sort [9,3,4,5,612,7,0,87,9,1]
+sumOfLists = zipWith (+) [1,2,3,4,5] [6,7,8,9,0,213]
+sumOfLists2 = zipWith (+) [6,7,8,9,0,213] [1,2,3,4,5]
+listBiggerThen5  = filter(>5) morePrimes
+evensUpTo20 = takeWhile (<=20) [2,4..]
+multOfList = foldl (*) 1 [2,3,4,5]
+multOfListRight = foldr (*) 1 [2,3,4,5]
+pow3List = [3^n | n <- [1..10]]
+multTable = [[x * y | y <- [1..10]] | x <- [1..10]]
--- tuples.hs
+++ tuples.hs
@@ -1,4 +1,7 @@
 import Data.List
 import System.IO
 
-randTuple
+randTuple = (1, "Random Tuple")
+bobSmith = ("Bob Smith", 52)
+bobsName = fst bobSmith -- extract the first component of pair
+bobsAge = snd bobSmith -- extract the second component of pair
