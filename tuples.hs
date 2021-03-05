b7a44c0ad776b65463946d2d9b1178204899806d
Matheus Pereira dos Santos <msantos.2015@alunos.utfpr.edu.br>
2021-03-04 22:20:16 -0300

    zip two lists

--- tuples.hs
+++ tuples.hs
@@ -5,3 +5,7 @@ randTuple = (1, "Random Tuple")
 bobSmith = ("Bob Smith", 52)
 bobsName = fst bobSmith -- extract the first component of pair
 bobsAge = snd bobSmith -- extract the second component of pair
+names = ["Bobs", "Mar", "Tom", "Jerry"]
+address = ["123 Maine", "234 Norht", "S67 West", "890 South"]
+namesNAddress = zip names address -- out >> [("Bobs","123 Maine"),("Mar","234 Norht"),("Tom","S67 West"),("Jerry","890 South")]
+
