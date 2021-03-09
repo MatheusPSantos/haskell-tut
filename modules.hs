d39ace24859db9c8ea4b6980e462af76606a038d
Matheus Pereira dos Santos <msantos.2015@alunos.utfpr.edu.br>
2021-03-08 23:39:08 -0300

    how to create and import module

---
+++ modules.hs
@@ -0,0 +1,8 @@
+import Data.List
+import System.IO
+
+-- basic exemplte of modules
+ 
+module SampFunctions (getClass, doubleEvenNumbers) where -- declaration of modules
+
+import SampFunctions -- importing module
